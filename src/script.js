5af6a19a8858b8cad8233c78a7a04287270c5565
nihalanand24 <nihalanand@gmail.com>
2021-03-23 00:02:35 -0400

    Refactored code
    - Created sass file structure
    - added profile section
    - changed bg from black to #080808
    - added gradient to blend header bg into rest of body

---
+++ src/script.js
@@ -0,0 +1,66 @@
+
+
+window.addEventListener("mousemove", (e) => {
+    width = document.documentElement.clientWidth,
+    height = document.documentElement.clientHeight,
+
+      percentX = Math.round((event.pageX / width) * 100);
+    percentY = Math.round((event.pageY / height) * 100);
+
+    const rg = document.querySelector(".radial-gradient");
+
+    rg.style.background = `radial-gradient(circle at 
+    ${percentX}% ${percentY}%, 
+    rgb(0, 99, 99),
+        black,
+        black,
+        black,
+        black)`;
+
+  });
+
+  const addTrianglifyBG = () => {
+
+    const removeIfExists = (element) => {
+      const elementToRemove = document.querySelector(element);
+
+      if(document.querySelector(element)) {
+        document.body.removeChild(elementToRemove);
+      }
+    }
+
+    removeIfExists('.trianglify-bg');
+    removeIfExists('.trianglify-overlay');
+
+    const pattern = trianglify({
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+      xColors: ["222a", "1118", "0008", '1118', '222a'],
+      yColors: 'match',
+      cellSize: 80,
+      strokeWidth: 1.51,
+      variance: 1,
+      seed: "My name is Nihal Anand, and I am a web developer.",
+      // colorFunction: trianglify.colorFunctions.sparkle()
+    });
+
+    const bgPattern = pattern.toCanvas();
+    bgPattern.classList.add("trianglify-bg");
+
+    const bgOverlay = document.createElement('div');
+    bgOverlay.classList.add('trianglify-overlay');
+
+    const addBG = (bg, overlay) => {
+      document.body.appendChild(bg);
+      document.body.appendChild(overlay);
+    };
+
+    addBG(bgPattern, bgOverlay);
+  }
+
+  addTrianglifyBG();
+
+
+  window.addEventListener("resize", () => {
+    addTrianglifyBG();
+  });
